52e84a850688da05975edf2b8cabd49bcf2ebd41
irakligeo <i.lataria@geolab.edu.ge>
2016-03-26 23:12:14 +0400

    1111

---
+++ MyApplication/app/src/main/java/com/example/geolab/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.geolab.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.geolab.myapplication.Activities.News;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(MainActivity.this, News.class);
+                startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, 2000);
+    }
+}
